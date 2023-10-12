d0bc02f8c09b9c67c452a983968f6da9c13552e8
Pranjal Goyal <id4pranjal@gmail.com>
2023-10-13 01:43:30 +0530

    removed lib/nav

--- src/App.js
+++ src/App.js
@@ -4,23 +4,22 @@ import Home from "./pages/Home";
 import OurTeam from "./pages/OurTeam";
 import Events from "./pages/Events";
 import Navbar from "./components/Navbar";
-import { ReactNavbar } from "./lib/overlay-nav";
 import NavBar from "./components/burgerMenu";
 
 function App() {
-    return (
-        <>
-            <div className=" absolute z-[40] right-5 lg:right-20 top-5 ">
-                <NavBar />
-            </div>
-            <Routes>
-                <Route exact path="/" element={<Home />}></Route>
-                <Route exact path="/Home" element={<Home />}></Route>
-                <Route exact path="/team" element={<OurTeam />}></Route>
-                <Route exact path="/events" element={<Events />}></Route>
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <div className=" absolute z-[40] right-5 lg:right-20 top-5 ">
+        <NavBar />
+      </div>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/Home" element={<Home />}></Route>
+        <Route exact path="/team" element={<OurTeam />}></Route>
+        <Route exact path="/events" element={<Events />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
